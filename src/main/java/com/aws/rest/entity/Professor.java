89f2c9f8728d8bf517c4f95d2d14c312771135cd
Victor Lavalle <vl@wayakna.com>
2022-05-17 19:25:09 -0500

    ✨ Refactoring entities and added JPA notations
    Deleted code that lombok does

--- src/main/java/com/aws/rest/entity/Professor.java
+++ src/main/java/com/aws/rest/entity/Professor.java
@@ -1,6 +1,8 @@
 package com.aws.rest.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -8,17 +10,19 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
-@Entity
-@Table(name = "profesor")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "profesor")
 public class Professor {
 
     private static final long serialVersionUID = 1L;
 
-    /*    @NotNull(message = "Field must not be empty")
-        @PositiveOrZero(message = "ID cannot be negative")*/
     @Id
+    @NotNull(message = "Field must not be empty")
+    @PositiveOrZero(message = "ID cannot be negative")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_profesor")
     private Long id;
@@ -37,81 +41,6 @@ public class Professor {
     @PositiveOrZero(message = "Class hours cannot be negative")
     private int horasClase;
 
-    /**
-     * Constructors
-     */
-    public Professor() {
-    }
-
-    public Professor(int numeroEmpleado, String nombres, String apellidos, int horasClase) {
-        this.numeroEmpleado = numeroEmpleado;
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.horasClase = horasClase;
-    }
-
-    public Professor(Long id, int numeroEmpleado, String nombres, String apellidos, int horasClase) {
-        this.id = id;
-        this.numeroEmpleado = numeroEmpleado;
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.horasClase = horasClase;
-    }
-
-/*    *//**
-     * Professor's id Getter & Setter
-     *//*
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    *//**
-     * Professor's employeeNumber Getter & Setter
-     *//*
-    public int getNumeroEmpleado() {
-        return numeroEmpleado;
-    }
-
-    public void setNumeroEmpleado(int numeroEmpleado) {
-        this.numeroEmpleado = numeroEmpleado;
-    }
-
-    *//**
-     * Professor's name Getter & Setter
-     *//*
-    public String getNombres() {
-        return nombres;
-    }
-
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
-
-    *//**
-     * Professor's last name Getter & Setter
-     *//*
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    *//**
-     * Professor's class hours Getter & Setter
-     *//*
-    public int getHorasClase() {
-        return horasClase;
-    }
-
-    public void setHorasClase(int horasClase) {
-        this.horasClase = horasClase;
-    }*/
 
     @Override
     public String toString() {
--- src/main/java/com/aws/rest/entity/Student.java
+++ src/main/java/com/aws/rest/entity/Student.java
@@ -1,13 +1,27 @@
 package com.aws.rest.entity;
 
+import lombok.*;
+import javax.persistence.*;
+import java.io.Serializable;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "estudiante")
 public class Student {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
     @NotNull(message = "Field must not be empty")
     @PositiveOrZero(message = "ID cannot be negative")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_estudiante")
     private long id;
 
     @NotEmpty(message = "Field must not be empty")
@@ -23,81 +37,9 @@ public class Student {
     @PositiveOrZero (message = "Average cannot be negative")
     private double promedio;
 
+    @NotNull (message = "Missing value")
+    private String fotoPerfilUrl;
 
-    /**
-     * Constructors
-     */
-    public Student() {}
-
-    public Student(String matricula, String nombres, String apellidos, double promedio) {
-        this.matricula = matricula;
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.promedio = promedio;
-    }
-
-    public Student(long id, String matricula, String nombres, String apellidos, double promedio) {
-        this.id = id;
-        this.matricula = matricula;
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.promedio = promedio;
-    }
-
-    /**
-     * id Getter & Setter
-     */
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    /**
-     * matricula Getter & Setter
-     */
-    public String getMatricula() {
-        return matricula;
-    }
-
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-
-    /**
-     * Student's name Getter & Setter
-     */
-    public String getNombres() {
-        return nombres;
-    }
-
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
-
-    /**
-     * Student's nombres Getter & Setter
-     */
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    /**
-     * Student's average Getter & Setter
-     */
-    public double getPromedio() {
-        return promedio;
-    }
-
-    public void setPromedio(double promedio) {
-        this.promedio = promedio;
-    }
 
     /**
      * Student's data
